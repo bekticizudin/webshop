42d909669abfb54a95986add9ceef4ad0938c2ab
bekticizudin <izudinbektic30@gmail.com>
2023-08-27 01:50:22 +0200

    adding contact form

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() {}
+  constructor() {
+  }
 
 ngOnInit(): void {
+
 }
 
 
+
 }
+
